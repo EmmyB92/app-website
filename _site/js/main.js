ef6e561d467e2a1ec19b6c1432b703ce2f4071fb
EmmyB92 <emily_is_kewl@Emilys-MacBook-Pro-2.local>
2014-01-27 22:32:10 -0500

    JEKYLL time!

---
+++ _site/js/main.js
@@ -0,0 +1,60 @@
+var $items = $('.items img');
+
+var switchItem = function (current, incoming) {
+	$items.eq(incoming).attr('data-state', 'incoming').fadeIn(250, function () {
+		$items.eq(current).hide().attr('data-state', '');
+		$items.eq(incoming).attr('data-state', 'current');
+	});
+};
+
+$items.filter(':not([data-state="current"])').hide();
+
+$('.next').on('click', function () {
+	var current = $items.filter('[data-state="current"]').index();
+	var next = current + 1;
+	
+	if (next > $items.length - 1) {
+		next = 0;
+	}
+	
+	switchItem(current, next);
+
+});
+
+$('.previous').on('click', function () {
+	var current = $items.filter('[data-state="current"]').index();
+	var previous = current - 1;
+	
+	if (previous > $items.length + 1) {
+		previous = 0;
+	}
+	
+	switchItem(current, previous);
+
+});
+
+var $videoDialog = $('dialog');
+dialogPolyfill.registerDialog($videoDialog.get(0));
+
+$('.btn-open').on('click', function () {
+	$videoDialog.children('.video').html('<iframe src="http://player.vimeo.com/video/68787849?color=c9ff23&amp;autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+	
+	/* When triggering built-in Javascript functions we need to bypass jQuery. In order to get to the raw Javascript element we can use .get()
+	Ex: $videoDialog.show() – will trigger jQuery's show function not the raw Javascript show function
+	$videoDialog.get(0).show() – will get the first raw Javascript element and trigger its native show function
+	*/
+
+
+	$videoDialog.get(0).showModal();
+	/* .show() allows users to interact with stuff behind the dialog
+	.showModal() disables user interaction behind the dialog
+	
+	
+	$videoDialog.get(0).show();
+	*/
+});
+
+$('.btn-close').on('click', function () {
+ $videoDialog.get(0).close();
+ $videoDialog.children('.video').html('');
+});
